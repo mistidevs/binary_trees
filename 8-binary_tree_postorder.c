d50d2f965b90eb0c3d149bc6978b97ab5b1922fc
mistidevs-alx <mistidevs@gmail.com>
2024-01-03 09:02:23 -0800

    Post-order traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_postorder - traversing a binary tree using
+* pre-order method
+* @tree: binary tree to traverse
+* @func: print number function
+*/
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree == NULL || func == NULL)
+	return;
+
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+func(tree->n);
+}
