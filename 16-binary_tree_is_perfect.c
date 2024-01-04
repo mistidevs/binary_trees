8d23b68dede4cc7046bd1de6420aee34299e92ec
mistidevs-alx <mistidevs@gmail.com>
2024-01-04 02:14:22 -0800

    Binary tree is perfect Fix #1

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,47 +1,53 @@
 #include "binary_trees.h"
 
 /**
- * _height - Measures the height of a binary tree
- *
- * @tree: Pointer to the node to measures the height
- *
- * Return: The height of the tree starting at @node
+ * tree_is_perfect - function that says if a tree is perfect or not
+ * it has to be the same quantity of levels in left as right, and also
+ * each node has to have 2 nodes or none
+ * @tree: tree to check
+ * Return: 0 if is not a perfect or other number that is the level of height
  */
-
-int _height(const binary_tree_t *tree)
+int tree_is_perfect(const binary_tree_t *tree)
 {
-int height_l;
-int height_r;
+int l = 0, r = 0;
 
-height_l = tree->left ? 1 + _height(tree->left) : 0;
-height_r = tree->right ? 1 + _height(tree->right) : 0;
-return (height_l > height_r ? height_l : height_r);
+if (tree->left && tree->right)
+{
+	l = 1 + tree_is_perfect(tree->left);
+	r = 1 + tree_is_perfect(tree->right);
+	if (r == l && r != 0 && l != 0)
+		return (r);
+	return (0);
 }
-
+else if (!tree->left && !tree->right)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
 /**
-* binary_tree_is_perfect - checks if binary tree is perfect
-* @tree: tree or subtree to check
-*
-* Return: 0 if not perfect; 1 if perfect
-*/
-
+ * binary_tree_is_perfect - perfect or not a tree
+ * @tree: tree to check
+ * Return: 1 is it is or 0 if not
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-int left_height = 0, right_height = 0;
+int result = 0;
 
 if (tree == NULL)
+{
 	return (0);
-
-if (tree->left == NULL && tree->right == NULL)
-	return (1);
-
-left_height = _height(tree->left);
-right_height = _height(tree->right);
-
-if (tree->left == NULL || tree->right == NULL)
-	return (left_height && right_height &&
-	tree->left == NULL && tree->right == NULL);
-
-return ((left_height == right_height) &&
-binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+}
+else
+{
+	result = tree_is_perfect(tree);
+	if (result != 0)
+	{
+		return (1);
+	}
+	return (0);
+}
 }
