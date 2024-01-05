e30368a27dd7be51162e17f9566f43d9334224e9
mistidevs-alx <mistidevs@gmail.com>
2024-01-05 00:54:50 -0800

    BST insert

--- binary_trees.h
+++ binary_trees.h
@@ -14,13 +14,18 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /**** Generic print binary tree function ***/
 void binary_tree_print(const binary_tree_t *);
@@ -55,5 +60,6 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
 #endif
